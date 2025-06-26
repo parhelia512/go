5f81a147f795ed3730e0d7ed98d10853a17ab0e9
parhelia512 <0011d3@gmail.com>
2025-06-26 21:08:14 +0800

    Revert "crypto/rand,runtime: switch RtlGenRandom for ProcessPrng"
    This reverts commit 693def151adff1af707d82d28f55dba81ceb08e1.

--- src/crypto/internal/sysrand/rand_windows.go
+++ src/crypto/internal/sysrand/rand_windows.go
@@ -6,6 +6,22 @@ package sysrand
 
 import "internal/syscall/windows"
 
+// RtlGenRandom only returns 1<<32-1 bytes at a time. We only read at
+// most 1<<31-1 bytes at a time so that  this works the same on 32-bit
+// and 64-bit systems.
+const maxSize = 1<<31-1
+
 func read(b []byte) error {
-	return windows.ProcessPrng(b)
+	for len(b) > 0 {
+		size := len(b)
+		if size > maxSize {
+			size = maxSize
+		}
+		err := windows.RtlGenRandom(b[:size])
+		if err != nil {
+			return err
+		}
+		b = b[size:]
+	}
+	return nil
 }
